125904b97e23dd2d6954c59111b6f2bc1a2676f5
Valkyrie8492 <ryanshepherd8492@gmail.com>
2024-12-23 19:08:23 -0800

    Created the necessary files for a functioning webpage and setup the logic for the game

---
+++ rps.js
@@ -0,0 +1,15 @@
+// breaking down rock paper scissors
+
+// create a function that generates a computer choice
+function getComputerChoice() {
+    const randVal = Math.random()
+    
+}
+// create a function that asks for the human choice
+// assign the input of each choice from the functions to a variable
+
+// compare the two choices to determine a round winner + redo the round if there's a tie
+
+// create a tracker for computer and human scores across the game
+
+// determine a game winner based on most rounds won across 5 rounds

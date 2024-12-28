9ac3af77d39f1b61d20126c560a19682769998b2
Valkyrie8492 <ryanshepherd8492@gmail.com>
2024-12-27 20:15:15 -0800

    finalized the logic such that it works and spaced it for readability

--- rps.js
+++ rps.js
@@ -31,15 +31,14 @@ function getHumanChoice() {
 // assign the input of each choice from the functions to a variable
 // maybe not if the function can just be called inside the game for a result
 
-const humanChoice = getHumanChoice()
-const computerChoice = getComputerChoice()
+// removed the human and computer choices as global variables so they would call the function for each round
 
 // compare the two choices to determine a round winner + redo the round if there's a tie
 // create a tracker for computer and human scores across the game
 // determine a game winner based on most rounds won across 5 rounds
 
-function playGame() {
-    function playRound(humanChoice, computerChoice) { // main round structure
+function playGame() { 
+    function playRound(humanChoice, computerChoice) { // main round structure        
         if (humanChoice === computerChoice) {
             return 0 // it's a tie
         } else if (humanChoice === "rock" && computerChoice === "scissors" ||
@@ -57,7 +56,9 @@ function playGame() {
 
     while (rounds < 5) {
         console.log(`~~~~~ Round ${rounds + 1} ~~~~~`)
-
+        
+        let humanChoice = getHumanChoice()
+        let computerChoice = getComputerChoice()
         let roundResult = playRound(humanChoice, computerChoice);
 
         if (roundResult === 1) {   // check scoring after each round
@@ -73,6 +74,18 @@ function playGame() {
         console.log(`You threw ${humanChoice} and the computer threw ${computerChoice}.`)  // show each of the throws
         console.log(`Your score: ${humanScore} Computer score: ${computerScore}`) // show the current scores
         rounds++ // increment rounds
+    }
+    
+    console.log("~~~~~ GAME OVER! ~~~~~") // end game, run the results
 
+    if (humanScore > computerScore) {
+        return `You've won! Final score is player: ${humanScore} computer: ${computerScore}`
+    } else if (computerScore > humanScore) {
+        return `You've lost! Final score is player: ${humanScore} computer: ${computerScore}`
+    } else {
+        return "It's a tie!"
     }
 }
+
+let outcome = playGame()
+console.log(outcome)

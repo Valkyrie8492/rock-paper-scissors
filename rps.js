01a23d12fdf247d8895fd6cc77d2fd3d67f573bf
Valkyrie8492 <ryanshepherd8492@gmail.com>
2024-12-27 19:16:30 -0800

    added most of the main logic for the game and adjusted the html file to read js to the console correctly

--- rps.js
+++ rps.js
@@ -1,15 +1,78 @@
 // breaking down rock paper scissors
 
 // create a function that generates a computer choice
+
+console.log("~~~~~~~ Let's Play Rock Paper Scissors! ~~~~~~~")
+
 function getComputerChoice() {
-    const randVal = Math.random()
-    
+    let randVal = Math.floor(Math.random() * 3) // not sure if this calls the function properly yet; floor is weird
+    if (randVal === 0) {
+        return "rock";
+    } else if (randVal === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
+
 // create a function that asks for the human choice
+
+function getHumanChoice() {
+    while (true) {
+        let userChoice = prompt("What will you throw? (Rock, Paper, or Scissors: ").toLowerCase(); // finally an answer on how the lowercase function works w/ prompts
+        if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
+            return userChoice;
+        } else {
+            alert("Sorry, please choose between rock, paper, or scissors." )
+        }
+    }
+}
+
 // assign the input of each choice from the functions to a variable
+// maybe not if the function can just be called inside the game for a result
+
+const humanChoice = getHumanChoice()
+const computerChoice = getComputerChoice()
 
 // compare the two choices to determine a round winner + redo the round if there's a tie
-
 // create a tracker for computer and human scores across the game
-
 // determine a game winner based on most rounds won across 5 rounds
+
+function playGame() {
+    function playRound(humanChoice, computerChoice) { // main round structure
+        if (humanChoice === computerChoice) {
+            return 0 // it's a tie
+        } else if (humanChoice === "rock" && computerChoice === "scissors" ||
+                humanChoice === "scissors" && computerChoice === "paper" ||
+                humanChoice === "paper" && computerChoice === "rock") {
+            return 1
+        } else {
+            return 2
+        }
+    }
+
+    let humanScore = 0
+    let computerScore = 0
+    let rounds = 0
+
+    while (rounds < 5) {
+        console.log(`~~~~~ Round ${rounds + 1} ~~~~~`)
+
+        let roundResult = playRound(humanChoice, computerChoice);
+
+        if (roundResult === 1) {   // check scoring after each round
+            humanScore++;
+            console.log("You won this round!"); 
+        } else if (roundResult === 2) {
+            computerScore++;
+            console.log("Computer won this round!");
+        } else {
+            console.log("It's a tie!")
+        }
+
+        console.log(`You threw ${humanChoice} and the computer threw ${computerChoice}.`)  // show each of the throws
+        console.log(`Your score: ${humanScore} Computer score: ${computerScore}`) // show the current scores
+        rounds++ // increment rounds
+
+    }
+}
